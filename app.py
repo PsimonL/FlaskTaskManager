b18d07a3d51b25e0d7343466f5471b870490646c
Szymon Rogowski <szymon0r0official@gmail.com>
2022-09-07 18:05:44 +0200

    Adding delete, update and add buttons.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from turtle import title
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -20,17 +19,40 @@ class Todo(db.Model):
 def index():  # put application's code here
     # show all todos
     todo_list = Todo.query.all()
-    print(todo_list)
-    # return render_template("base.html", todo_list=todo_list)
-    return render_template("base.html")
+    # print(todo_list)
+    return render_template("base.html", todo_list=todo_list)
+    # return render_template("base.html")
 
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+# @app.route("/update/<int:todo_id>")
+# def update(todo_id):
+#     todo = Todo.query.filter_by(id=todo_id).first()
+#     todo.complete = not todo.complete
+#     db.session.commit()
+#     return redirect(url_for("index"))
+
+# @app.route("/delete/<int:todo_id>")
+# def delete(todo_id):
+#     todo = Todo.query.filter_by(id=todo_id).first()
+#     db.session.delete(todo)
+#     db.session.commit()
+#     return redirect(url_for("index"))
 
 if __name__ == '__main__':
     db.create_all()
-    new_todo = Todo(title="todo 1", complete=False)
-    db.session.add(new_todo)
-    db.session.commit()
+    # new_todo = Todo(title="todo 1", complete=False)
+    # db.session.add(new_todo)
+    # db.session.commit()
     app.run(debug=True, port=80)
 
-    #https://www.youtube.com/watch?v=Z1RJmh_OqeA&t=1207s
-    #https://www.youtube.com/watch?v=yKHJsLUENl0&t=389s
+    # https://www.youtube.com/watch?v=Z1RJmh_OqeA&t=1207s
+    # https://www.youtube.com/watch?v=yKHJsLUENl0&t=389s
