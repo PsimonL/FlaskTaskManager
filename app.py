705cf04ec0c23d20cf3f08543d4219a24fba2204
Szymon Rogowski <szymon0r0official@gmail.com>
2022-09-08 00:46:24 +0200

    Refractoring

--- app.py
+++ app.py
@@ -11,17 +11,15 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    complete = db.Column(db.Boolean)
+    title = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    complete = db.Column(db.Boolean, nullable=False)
 
 
 @app.route('/')
 def index():  # put application's code here
-    # show all todos
     todo_list = Todo.query.all()
-    # print(todo_list)
     return render_template("base.html", todo_list=todo_list)
-    # return render_template("base.html")
 
 
 @app.route("/add", methods=["POST"])
@@ -32,27 +30,23 @@ def add():
     db.session.commit()
     return redirect(url_for("index"))
 
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
 
-# @app.route("/update/<int:todo_id>")
-# def update(todo_id):
-#     todo = Todo.query.filter_by(id=todo_id).first()
-#     todo.complete = not todo.complete
-#     db.session.commit()
-#     return redirect(url_for("index"))
-
-# @app.route("/delete/<int:todo_id>")
-# def delete(todo_id):
-#     todo = Todo.query.filter_by(id=todo_id).first()
-#     db.session.delete(todo)
-#     db.session.commit()
-#     return redirect(url_for("index"))
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 if __name__ == '__main__':
     db.create_all()
-    # new_todo = Todo(title="todo 1", complete=False)
-    # db.session.add(new_todo)
-    # db.session.commit()
     app.run(debug=True, port=80)
 
-    # https://www.youtube.com/watch?v=Z1RJmh_OqeA&t=1207s
-    # https://www.youtube.com/watch?v=yKHJsLUENl0&t=389s
+    #https://www.youtube.com/watch?v=Z1RJmh_OqeA&t=1207s
+    #https://www.youtube.com/watch?v=yKHJsLUENl0&t=389s
